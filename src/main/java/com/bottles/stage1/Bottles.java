c2b9929323cdba3810a655f7e40f68a78b265fe6
Joohyung Park <joohyung.park@on-the-ground.org>
2026-06-22 13:07:20 +0900

    feat: introduce diffByRule

--- src/main/java/com/bottles/stage1/Bottles.java
+++ src/main/java/com/bottles/stage1/Bottles.java
@@ -2,63 +2,70 @@ import main.java.com.bottles.stage1.FileOut;
 import main.java.com.bottles.stage1.Output;
 import main.java.com.bottles.stage1.Stdout;
 
-static final class BottlesUtils {
+static final class NinetyNineBottles {
 
     private static final int MAX_BOTTLES = 99;
     private static final int INIT_STORE_STOCK = 101;
-    private static int storeStock = INIT_STORE_STOCK;
+    private int storeStock;
 
-    private static String verse(int number) {
-        if (number == 1) {
-            return "1 bottle of beer on the wall, 1 bottle of beer.\n"
-                    + "Take it down and pass it around, no more bottles of beer on the wall.\n";
-        }
-
-        if (number == 2) {
-            return "2 bottles of beer on the wall, 2 bottles of beer.\n"
-                    + "Take one down and pass it around, 1 bottle of beer on the wall.\n";
-        }
-
-        return number + " bottles of beer on the wall, "
-                + number + " bottles of beer.\n"
-                + "Take one down and pass it around, "
-                + (number - 1)
-                + " bottles of beer on the wall.\n";
+    public NinetyNineBottles() {
+        storeStock = INIT_STORE_STOCK;
     }
 
-    private static String verse0(int newStock) {
-        if (newStock > 0) return "No more bottles of beer on the wall, no more bottles of beer.\n" +
-                "Go to the store and buy some more, " + newStock + " bottles of beer on the wall.\n\n";
-        return "No more bottles of beer on the wall, no more bottles of beer.\n" +
-                "Even the store has no more bottles. Time to say goodbye, my dear.\n\n";
+    private static String numBottles(int num) {
+        if (num == 0) return "no more bottles";
+        if (num == 1) return num + " bottle";
+        return num + " bottles";
     }
 
-    private static int orderBottles() {
+    private static String numBottlesCapitalized(int num) {
+        if (num == 0) return "No more bottles";
+        if (num == 1) return num + " bottle";
+        return num + " bottles";
+    }
+
+    private static String verse(int curBottles, int numDiff) {
+        String currentBottles = numBottlesCapitalized(curBottles) + " of beer on the wall, " + numBottles(curBottles) + " of beer.\n";
+        if (numDiff < 0) { // Take bottles down
+            return currentBottles + "Take " + (-numDiff) + " down and pass " + ((-numDiff == 1) ? "it" : "them") + " around, "
+                    + numBottles(curBottles + numDiff) + " of beer on the wall.\n";
+        } else if (numDiff > 0) { // Order more Bottles
+            return currentBottles + "Go to the store and buy some more, " + numBottles(curBottles + numDiff) + " of beer on the wall.\n";
+        }
+        // No more bottles
+            return currentBottles + "Even the store has no more bottles. Time to say goodbye, my dear.\n";
+    }
+
+    private int orderBottles() {
         int res = Math.min(storeStock, MAX_BOTTLES);
         storeStock -= res;
         return res;
     }
 
-    private static int iteration(Output output, int initBottles) {
-        for (int i = initBottles; i >= 1; i--) {
-            output.out(verse(i));
-        }
-        int newStock = orderBottles();
-        output.out(verse0(newStock));
-        return newStock;
+
+    private int diffByRule(int curBottles) {
+        final int maxUnload = 2;
+        final int multiUnloadThreshold = 10;
+        if (curBottles == 0) return orderBottles();
+        if (curBottles < maxUnload) return -curBottles;
+        if (curBottles <= multiUnloadThreshold) return -maxUnload;
+        return -1;
     }
 
-    public static void song(Output output) {
-        int newStock = MAX_BOTTLES;
-        while (newStock > 0) {
-            newStock = iteration(output, newStock);
-        }
+    public void song(Output output) {
+        int curBottles = MAX_BOTTLES;
+        int diff;
+        do {
+            diff = diffByRule( curBottles);
+            output.out(verse(curBottles, diff));
+            curBottles += diff;
+        } while (diff != 0);
     }
 }
 
 void main() throws IOException {
-    BottlesUtils.song(Stdout.INSTANCE);
+    new NinetyNineBottles().song(Stdout.INSTANCE);
     try (FileOut file = new FileOut("99bottles.output")) {
-        BottlesUtils.song(file);
+        new NinetyNineBottles().song(file);
     }
 }
